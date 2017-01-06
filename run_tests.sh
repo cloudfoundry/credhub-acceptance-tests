7f530b71c5a059d170455f87420d0ec6de309e5f
Isobel Redelmeier <iredelmeier@pivotal.io>
2017-01-06 11:47:23 -0800

    Parallelize the acceptance tests
    Signed-off-by: Andrew Cantino <acantino@pivotal.io>

--- run_tests.sh
+++ run_tests.sh
@@ -4,5 +4,5 @@ if [ ! -e config.json ]; then
   echo "You are missing config.json - use ./target_local.sh to point to your local Credhub."
   exit 1
 else
-ginkgo -r integration
+ginkgo -r -p integration
 fi
