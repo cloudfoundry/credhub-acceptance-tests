9b3397f151b261cba84e163d88ebfb58b4b86f03
Isobel Redelmeier <iredelmeier@pivotal.io>
2017-02-09 15:02:09 -0800

    Add a race_condition test
    Two requests, separated by 150 ms, send a request to generate a secret
    with --no-overwrite flag should return the same secret
    
    Signed-off-by: Kelly Gerritz <kgerritz@pivotal.io>
    Signed-off-by: Sebastian Wittenkamp <swittenkamp@pivotal.io>
    Signed-off-by: Iryna Shustava <ishustava@pivotal.io>

---
+++ integration_test/race_condition_test.go
@@ -0,0 +1,44 @@
+package integration_test
+
+import (
+	"time"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/gexec"
+)
+
+var _ = FDescribe("Race condition tests", func() {
+	Describe("when creating a new secret in multiple threads with `--no-overwrite`", func() {
+		It("should return the same value for both given they are seperated by at least 150 milliseconds", func() {
+			rsaSecretName := generateUniqueCredentialName()
+
+			waitForSession1 := make(chan *Session)
+			waitForSession2 := make(chan *Session)
+
+			go func() {
+				time.Sleep(150 * time.Millisecond)
+				session := runCommand("generate", "-n", rsaSecretName, "-t", "rsa", "--no-overwrite")
+				waitForSession1 <- session
+			}()
+
+			go func() {
+				session := runCommand("generate", "-n", rsaSecretName, "-t", "rsa", "--no-overwrite")
+				waitForSession2 <- session
+			}()
+
+			session1 := <- waitForSession1
+			session2 := <- waitForSession2
+
+			Eventually(session1).Should(Exit(0))
+			Eventually(session2).Should(Exit(0))
+			stdOut1 := string(session1.Out.Contents())
+			stdOut2 := string(session2.Out.Contents())
+
+			Expect(stdOut1).To(MatchRegexp(`Type:\s+rsa`))
+			Expect(stdOut1).To(MatchRegexp(`Public Key:\s+-----BEGIN PUBLIC KEY-----`))
+			Expect(stdOut1).To(MatchRegexp(`Private Key:\s+-----BEGIN RSA PRIVATE KEY-----`))
+
+			Expect(stdOut1).To(Equal(stdOut2))
+		})
+	})
+});
