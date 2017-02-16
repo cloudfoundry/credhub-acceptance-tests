17cacbc10cc28d769c3537778de109c44c6aea35
Andrew Cantino <acantino@pivotal.io>
2017-02-16 14:01:47 -0800

    Add a race condition test for 'set' command
    [#137494995] `--no-overwrite` can overwrite
    
    Signed-off-by: Phil Goodwin <pgoodwin@pivotal.io>

--- integration_test/race_condition_test.go
+++ integration_test/race_condition_test.go
@@ -9,8 +9,8 @@ import (
 )
 
 var _ = Describe("Race condition tests", func() {
-	Describe("when creating a new secret in multiple threads with `--no-overwrite`", func() {
-		It("should return the same value for both given they are seperated by at least 150 milliseconds", func() {
+	Describe("when generating a new secret in multiple threads with `--no-overwrite`", func() {
+		It("should return the same value for both", func() {
 			rsaSecretName := generateUniqueCredentialName()
 
 			waitForSession1 := make(chan *Session)
@@ -38,6 +38,35 @@ var _ = Describe("Race condition tests", func() {
 			Expect(stdOut1).To(MatchRegexp(`Type:\s+rsa`))
 			Expect(stdOut1).To(MatchRegexp(`Public Key:\s+-----BEGIN PUBLIC KEY-----`))
 			Expect(stdOut1).To(MatchRegexp(`Private Key:\s+-----BEGIN RSA PRIVATE KEY-----`))
+
+			Expect(stdOut1).To(Equal(stdOut2))
+		})
+	})
+
+	Describe("when setting a new secret in multiple threads with `--no-overwrite`", func() {
+		It("should return the same value for both", func() {
+			rsaSecretName := generateUniqueCredentialName()
+
+			waitForSession1 := make(chan *Session)
+			waitForSession2 := make(chan *Session)
+
+			go func() {
+				session := runCommand("set", "-n", rsaSecretName, "-v", "test-value", "--no-overwrite")
+				waitForSession1 <- session
+			}()
+
+			go func() {
+				session := runCommand("set", "-n", rsaSecretName, "-v", "test-value", "--no-overwrite")
+				waitForSession2 <- session
+			}()
+
+			session1 := <-waitForSession1
+			session2 := <-waitForSession2
+
+			Eventually(session1).Should(Exit(0))
+			Eventually(session2).Should(Exit(0))
+			stdOut1 := string(session1.Out.Contents())
+			stdOut2 := string(session2.Out.Contents())
 
 			Expect(stdOut1).To(Equal(stdOut2))
 		})
