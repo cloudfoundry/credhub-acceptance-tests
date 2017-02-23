e2357cbe72789abc100ed7c3ac1626d6eb3eafcf
Andrew Cantino <acantino@pivotal.io>
2017-02-22 16:01:39 -0800

    Regression test showing that set clears the ca field
    [#140446447] Audit set handlers to ensure all unspecified fields are set to null (or equivalent)
    
    Signed-off-by: Isobel Redelmeier <iredelmeier@pivotal.io>

--- integration_test/updating_a_secret_test.go
+++ integration_test/updating_a_secret_test.go
@@ -30,4 +30,35 @@ var _ = Describe("updating a secret", func() {
 			})
 		})
 	})
+
+	Describe("generating -> setting -> generating", func() {
+		It("does not bleed values from the generate", func() {
+			caName := generateUniqueCredentialName()
+			credentialname := generateUniqueCredentialName()
+
+			By("generating a new ca", func() {
+				runCommand("generate", "-n", caName, "-t", "certificate", "-c", "anything", "--is-ca", "--self-sign")
+			})
+
+			By("generating a new certificate signed by the CA", func() {
+				session := runCommand("generate", "-n", credentialname, "-t", "certificate", "-c", "bla", "--ca", caName)
+				stdOut := string(session.Out.Contents())
+				Eventually(session).Should(Exit(0))
+				Expect(stdOut).To(MatchRegexp(`Type:\s+certificate`))
+				Expect(stdOut).To(MatchRegexp(`Certificate:\s+-----BEGIN CERTIFICATE-----`))
+				Expect(stdOut).To(MatchRegexp(`Ca:\s+-----BEGIN CERTIFICATE-----`))
+				Expect(stdOut).To(MatchRegexp(`Private Key:\s+-----BEGIN RSA PRIVATE KEY-----`))
+			})
+
+			By("overwriting the certificate with `set`", func() {
+				session := runCommand("set", "-n", credentialname, "-t", "certificate", "--certificate-string", "fake-certificate")
+				stdOut := string(session.Out.Contents())
+				Eventually(session).Should(Exit(0))
+				Expect(stdOut).To(MatchRegexp(`Type:\s+certificate`))
+				Expect(stdOut).To(MatchRegexp(`Certificate:\s+fake-certificate`))
+				Expect(stdOut).To(Not(MatchRegexp(`Ca:\s+-----BEGIN CERTIFICATE-----`)))
+				Expect(stdOut).To(Not(MatchRegexp(`Private Key:\s+-----BEGIN RSA PRIVATE KEY-----`)))
+			})
+		})
+	})
 })
