69b8a6767727c9b3339290e02f4c9cbf868db2e3
Iryna Shustava <ishustava@pivotal.io>
2017-02-28 14:02:27 -0800

    SSH public key must be base64 decodable
    When setting ssh credential to a static string, that string has to be
    base64 decodable by the Java decoder, i.e. its length must be divisible
    by 3.
    
    [#139303837] SSH credentials should contain SHA256 hash of SSH key

--- integration_test/ssh_key_test.go
+++ integration_test/ssh_key_test.go
@@ -10,13 +10,14 @@ import (
 var _ = Describe("SSH key test", func() {
 	Describe("setting an SSH key", func() {
 		It("should be able to set an ssh key", func() {
-			session := RunCommand("set", "-n", GenerateUniqueCredentialName(), "-t", "ssh", "-U", "iamapublickey", "-P", credentialValue)
+			base64DecodablePublicKey := "public"
+			session := RunCommand("set", "-n", GenerateUniqueCredentialName(), "-t", "ssh", "-U", base64DecodablePublicKey, "-P", credentialValue)
 			stdOut := string(session.Out.Contents())
 
 			Eventually(session).Should(Exit(0))
 
 			Expect(stdOut).To(MatchRegexp(`Type:\s+ssh`))
-			Expect(stdOut).To(MatchRegexp(`Public Key:\s+iamapublickey`))
+			Expect(stdOut).To(MatchRegexp(`Public Key:\s+` + base64DecodablePublicKey))
 			Expect(stdOut).To(MatchRegexp("Private Key:\\s+" + credentialValue))
 		})
 	})
