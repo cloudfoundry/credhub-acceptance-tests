efe26e06d6f2f2f57cee20d88908fc4115ad09ce
Jacques Chester <jchester@pivotal.io>
2017-03-02 10:34:34 -0500

    Add script to target for tests
    [#139303837] SSH credentials should contain SHA256 hash of SSH key
    
    Signed-off-by: Andrew Cantino <acantino@pivotal.io>

---
+++ target_local.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+cat <<EOF > config.json
+{
+  "api_url": "http://localhost:9000",
+  "api_username":"${USERNAME}",
+  "api_password":"${PASSWORD}"
+}
+EOF
+echo "config.json now points to localhost:9000"
