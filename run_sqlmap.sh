b92b6f26c13cfd19cb600859e9cecfc9c9a0585a
Ashwin Venkatesh <avenkatesh@pivotal.io>
2017-06-07 11:49:33 -0400

    Dial down SQLMAP_LEVEL & SQLMAP_RISK
    [#146494991] Re-enable sqlmap check for /api/v1/data?paths=true
    
    Signed-off-by: Kelly Gerritz <kgerritz@pivotal.io>

--- run_sqlmap.sh
+++ run_sqlmap.sh
@@ -12,8 +12,8 @@ set -eu
 CREDENTIAL_ROOT="$(mktemp -d)"
 AUTH_FILE="${CREDENTIAL_ROOT}/auth_file.pem"
 # Defaults level and risk to maximum to increase test coverage
-SQLMAP_LEVEL="${SQLMAP_LEVEL:-5}"
-SQLMAP_RISK="${SQLMAP_RISK:-3}"
+SQLMAP_LEVEL="${SQLMAP_LEVEL:-3}"
+SQLMAP_RISK="${SQLMAP_RISK:-2}"
 
 BASEDIR="$(dirname ${0})"
 
