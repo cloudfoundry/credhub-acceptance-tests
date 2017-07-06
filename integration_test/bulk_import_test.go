fb283de72056daee0941550ad09b9f30e31e3361
Ashwin Venkatesh <avenkatesh@pivotal.io>
2017-07-06 16:12:53 -0400

    Acceptance test for certificate bulk import
    [#147953739] User should be able to bulk import certificate credentials from a file using the CLI
    
    Signed-off-by: Iryna Shustava <ishustava@pivotal.io>

--- integration_test/bulk_import_test.go
+++ integration_test/bulk_import_test.go
@@ -9,6 +9,8 @@ import (
 
 var _ = Describe("Import test", func() {
 	It("should import credentials from a file", func() {
+		RunCommand("generate", "-n", "ca-certificate", "-t", "certificate", "-c", "credhub-ca", "-o", "pivotal", "-u", "credhub", "-i", "nyc", "-s", "NY", "-y", "US", "--is-ca", "--self-sign")
+
 		session := RunCommand("import", "-f", "../test_helpers/bulk_import.yml")
 		Eventually(session).Should(Exit(0))
 
@@ -20,22 +22,52 @@ var _ = Describe("Import test", func() {
 		Expect(stdOut).To(ContainSubstring(`name: /director/deployment/blobstore - director`))
 		Expect(stdOut).To(ContainSubstring(`type: value`))
 		Expect(stdOut).To(ContainSubstring(`value: y14ck84ef51dnchgk4kp`))
+
+		Expect(stdOut).To(ContainSubstring(`name: /director/deployment/bosh-ca`))
+		Expect(stdOut).To(ContainSubstring(`type: certificate`))
+		Expect(stdOut).To(ContainSubstring(`value:`))
+		Expect(stdOut).To(ContainSubstring(`-----BEGIN CERTIFICATE-----`))
+		Expect(stdOut).To(ContainSubstring(`-----BEGIN RSA PRIVATE KEY-----`))
+
+		Expect(stdOut).To(ContainSubstring(`name: /director/deployment/bosh-cert`))
+		Expect(stdOut).To(ContainSubstring(`type: certificate`))
+		Expect(stdOut).To(ContainSubstring(`value:`))
+		Expect(stdOut).To(ContainSubstring(`-----BEGIN CERTIFICATE-----`))
+		Expect(stdOut).To(ContainSubstring(`-----BEGIN RSA PRIVATE KEY-----`))
 	})
 
 	It("should save the credentials on CredHub", func() {
-		session := RunCommand("get", "-n", "/director/deployment/blobstore - agent" )
+		session := RunCommand("get", "-n", "/director/deployment/blobstore - agent")
 		Eventually(session).Should(Exit(0))
 		stdOut := string(session.Out.Contents())
 		Expect(stdOut).To(ContainSubstring(`name: /director/deployment/blobstore - agent`))
 		Expect(stdOut).To(ContainSubstring(`type: password`))
 		Expect(stdOut).To(ContainSubstring(`value: gx4ll8193j5rw0wljgqo`))
 
-		session = RunCommand("get", "-n", "/director/deployment/blobstore - director" )
+		session = RunCommand("get", "-n", "/director/deployment/blobstore - director")
 		Eventually(session).Should(Exit(0))
 		stdOut = string(session.Out.Contents())
 		Expect(stdOut).To(ContainSubstring(`name: /director/deployment/blobstore - director`))
 		Expect(stdOut).To(ContainSubstring(`type: value`))
 		Expect(stdOut).To(ContainSubstring(`value: y14ck84ef51dnchgk4kp`))
+
+		session = RunCommand("get", "-n", "/director/deployment/bosh-ca")
+		Eventually(session).Should(Exit(0))
+		stdOut = string(session.Out.Contents())
+		Expect(stdOut).To(ContainSubstring(`name: /director/deployment/bosh-ca`))
+		Expect(stdOut).To(ContainSubstring(`type: certificate`))
+		Expect(stdOut).To(ContainSubstring(`value:`))
+		Expect(stdOut).To(ContainSubstring(`-----BEGIN CERTIFICATE-----`))
+		Expect(stdOut).To(ContainSubstring(`-----BEGIN RSA PRIVATE KEY-----`))
+
+		session = RunCommand("get", "-n", "/director/deployment/bosh-cert")
+		Eventually(session).Should(Exit(0))
+		stdOut = string(session.Out.Contents())
+		Expect(stdOut).To(ContainSubstring(`name: /director/deployment/bosh-cert`))
+		Expect(stdOut).To(ContainSubstring(`type: certificate`))
+		Expect(stdOut).To(ContainSubstring(`value:`))
+		Expect(stdOut).To(ContainSubstring(`-----BEGIN CERTIFICATE-----`))
+		Expect(stdOut).To(ContainSubstring(`-----BEGIN RSA PRIVATE KEY-----`))
 	})
 
 })
