2abb7e51ba5c0f59e7a25a7733df6dd083fb4a6e
Iryna Shustava <ishustava@pivotal.io>
2017-08-25 12:09:50 -0400

    Remove hardcoded API values in builder test
    [#150273573] cloudfoundry-incubator/credhub-cli #10: Implement CredHub API
    
    Signed-off-by: Kelly Gerritz <kgerritz@pivotal.io>

--- api_client_test/uaa_builder_test.go
+++ api_client_test/uaa_builder_test.go
@@ -3,11 +3,14 @@ package acceptance_test
 import (
 	"github.com/cloudfoundry-incubator/credhub-cli/credhub"
 	"github.com/cloudfoundry-incubator/credhub-cli/credhub/auth"
+	. "github.com/cloudfoundry-incubator/credhub-acceptance-tests/test_helpers"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("UaaBuilder", func() {
+	config, _ := LoadConfig()
+
 	Describe("AuthBuilder", func() {
 		It("builds an OAuthStrategy using existing tokens", func() {
 			oauth := credhubClient.Auth.(*auth.OAuthStrategy)
@@ -17,8 +20,8 @@ var _ = Describe("UaaBuilder", func() {
 			accessToken := oauth.AccessToken()
 			refreshToken := oauth.RefreshToken()
 
-			builder := auth.Uaa("credhub_cli", "", "credhub", "password", accessToken, refreshToken)
-			ch, err := credhub.New("https://localhost:9000", credhub.SkipTLSValidation(), credhub.Auth(builder))
+			builder := auth.Uaa("credhub_cli", "", config.ApiUsername, config.ApiPassword, accessToken, refreshToken)
+			ch, err := credhub.New(config.ApiUrl, credhub.SkipTLSValidation(), credhub.Auth(builder))
 			Expect(err).ToNot(HaveOccurred())
 
 			oauth = ch.Auth.(*auth.OAuthStrategy)
@@ -33,7 +36,7 @@ var _ = Describe("UaaBuilder", func() {
 	Describe("ClientCredentialsBuilder", func() {
 		It("builds an OAuthStrategy using client credentials", func() {
 			builder := auth.UaaClientCredentials("credhub_client", "secret")
-			ch, err := credhub.New("https://localhost:9000", credhub.SkipTLSValidation(), credhub.Auth(builder))
+			ch, err := credhub.New(config.ApiUrl, credhub.SkipTLSValidation(), credhub.Auth(builder))
 			Expect(err).ToNot(HaveOccurred())
 
 			_, err = ch.FindByPath("/something")
