c260030490daa52da0b46b269f867d7c52e9ba79
Iryna Shustava <ishustava@pivotal.io>
2017-09-27 16:06:08 -0700

    Add tests for FindByPartialName()
    [#150555821] Find by name and all paths functionality should work in go client

--- api_client_test/find_test.go
+++ api_client_test/find_test.go
@@ -6,12 +6,16 @@ import (
 
 	"github.com/cloudfoundry-incubator/credhub-cli/credhub/credentials"
 	"github.com/cloudfoundry-incubator/credhub-cli/credhub/credentials/generate"
+	"time"
+	"fmt"
+	"strconv"
 )
 
 var _ = Describe("Find", func() {
+	currentTime := time.Now().UnixNano()
 
-	passwordName1 := testCredentialPath("find-test/first-password")
-	passwordName2 := testCredentialPath("find-test/second-password")
+	passwordName1 := testCredentialPath(fmt.Sprint("find-test/first-password-", currentTime))
+	passwordName2 := testCredentialPath(fmt.Sprint("find-test/second-password-", currentTime))
 
 	var expectedPassword1 credentials.Password
 	var expectedPassword2 credentials.Password
@@ -37,6 +41,16 @@ var _ = Describe("Find", func() {
 
 	Specify("finding the credentials by path", func() {
 		results, err := credhubClient.FindByPath(testCredentialPrefix() + "find-test")
+
+		Expect(err).ToNot(HaveOccurred())
+
+		findResult1 := credentials.FindByNameResult{Name: passwordName1, VersionCreatedAt: expectedPassword1.VersionCreatedAt}
+		findResult2 := credentials.FindByNameResult{Name: passwordName2, VersionCreatedAt: expectedPassword2.VersionCreatedAt}
+		Expect(results.Credentials).To(ConsistOf(findResult1, findResult2))
+	})
+
+	Specify("finding the credentials by name-like", func() {
+		results, err := credhubClient.FindByPartialName(strconv.FormatInt(currentTime, 10))
 
 		Expect(err).ToNot(HaveOccurred())
 
