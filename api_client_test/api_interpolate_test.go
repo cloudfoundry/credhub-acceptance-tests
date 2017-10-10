ef1aa777191a7971f2f3c20811716745d1f02b37
Edie Beer <ebeer@pivotal.io>
2017-10-10 14:30:38 -0400

    add acceptance tests for interpolate and mtls golang lib support
    [#151720078] Merge cloudfoundry-incubator/credhub-cli #13: Adds API for interpolate endpoint

---
+++ api_client_test/api_interpolate_test.go
@@ -0,0 +1,43 @@
+package acceptance_test
+
+import (
+	"encoding/json"
+
+	"github.com/cloudfoundry-incubator/credhub-cli/credhub/credentials/values"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("InterpolateString", func() {
+	Specify("lifecycle", func() {
+		name := testCredentialPath("thing-to-interpolate")
+		cred := make(map[string]interface{})
+		cred["firstKey"] = "this is a value for the first key"
+
+		var unmarshalled values.JSON
+
+		_, err := credhubClient.SetJSON(name, cred, true)
+		Expect(err).ToNot(HaveOccurred())
+		keyValueJson := `{"firstKey":"this is a value for the first key"}`
+		json.Unmarshal([]byte(keyValueJson), &unmarshalled)
+
+		By("calling interpolateString with a valid credhub-ref")
+		vcapServicesString := `{"someOuterKey":[{"credentials": {"credhub-ref":"((` + name + `))"},"anotherKey":"some other value that does not change"}]}`
+		interpolatedString, err := credhubClient.InterpolateString(vcapServicesString)
+		Expect(err).ToNot(HaveOccurred())
+		Expect(interpolatedString).To(Equal(`{"someOuterKey":[{"anotherKey":"some other value that does not change","credentials":{"firstKey":"this is a value for the first key"}}]}`))
+		Expect(interpolatedString).NotTo(ContainSubstring("credhub-ref"))
+
+		By("calling interpolateString without credential angd credhub-ref returns input unchanged")
+		invalidString := `{"someOuterKey":[{"things":{"keyTwo":"this is not from credhub"},"anotherKey":"some other value that does not change"}]}`
+		interpolatedString, err = credhubClient.InterpolateString(invalidString)
+		Expect(err).ToNot(HaveOccurred())
+		Expect(interpolatedString).To(Equal(invalidString))
+
+		By("clean up from test")
+		err = credhubClient.Delete(name)
+		Expect(err).ToNot(HaveOccurred())
+		_, err = credhubClient.GetLatestJSON(name)
+		Expect(err).To(HaveOccurred())
+	})
+})
