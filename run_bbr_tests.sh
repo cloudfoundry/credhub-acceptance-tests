d46ee3ff7b59c3e31ee69c293bcb5cb4b83e43bf
Chhavi Kankaria <ckankaria@pivotal.io>
2018-01-30 10:21:04 -0500

    More typos
    Signed-off-by: Mo Lambo <mlambo@pivotal.io>

--- run_bbr_tests.sh
+++ run_bbr_tests.sh
@@ -16,7 +16,7 @@ cat <<EOF > test_config.json
     "bosh_ssh_username":"${BOSH_SSH_USERNAME}",
     "bosh_ssh_private_key_path":"${BOSH_SSH_PRIVATE_KEY_PATH}"
   },
-  "uaa_ca":"${SERVER_CA_CERT_PATH}"
+  "uaa_ca":"${SERVER_CA_CERT_PATH}",
   "client_name":"${CLIENT_NAME}",
   "client_secret":"${CLIENT_SECRET}"
 }
