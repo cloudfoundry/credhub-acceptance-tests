7ca8463bda061ac08eefef29e4e843cafdc906a1
Ben Moss <bmoss@pivotal.io>
2018-06-18 14:41:39 -0400

    Remove smoke tests

--- run_smoke_tests.sh
+++ run_smoke_tests.sh
@@ -2,16 +2,5 @@
 
 set -eu
 
-API_URL=${API_URL:-https://localhost:9000}
-USERNAME=${USERNAME:-credhub}
-PASSWORD=${PASSWORD:-password}
-
-cat <<EOF > test_config.json
-{
-  "api_url": "${API_URL}",
-  "api_username":"${USERNAME}",
-  "api_password":"${PASSWORD}"
-}
-EOF
-
-ginkgo -r -p smoke_test
+# Smoke tests aren't a thing on 1.3, just here for CI task compatibility
+exit 0
