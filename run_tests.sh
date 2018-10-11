4bfe7de1debb759f904ed140cc78f5f59a043d0d
Anna Thornton <athornton@pivotal.io>
2018-10-11 09:54:42 -0400

    Fix acceptance tests

--- run_tests.sh
+++ run_tests.sh
@@ -6,7 +6,7 @@ API_URL=${API_URL:-https://localhost:9000}
 USERNAME=${USERNAME:-credhub}
 PASSWORD=${PASSWORD:-password}
 CREDENTIAL_ROOT=${CREDENTIAL_ROOT:-~/workspace/credhub-release/src/credhub/src/test/resources}
-UAA_CA=${UAA_CA:-~/workspace/credhub-deployments/ca/credhub_root_ca.pem}
+UAA_CA=${UAA_CA:-~/workspace/credhub-deployments/ca/uaa_ca.pem}
 CLIENT_NAME=${CLIENT_NAME:-credhub_client}
 CLIENT_SECRET=${CLIENT_SECRET:-secret}
 
