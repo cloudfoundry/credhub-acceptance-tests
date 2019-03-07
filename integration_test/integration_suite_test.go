423cb4743b3533e805cf897b6901a429904ca7d5
Tom Kennedy <tkennedy@pivotal.io>
2019-03-07 16:55:09 -0500

    Revert "Update acceptance tests to check pre-built cli in ci"
    This reverts commit 79914863eaffd348e949e0249f73e96b88bae776.

--- integration_test/integration_suite_test.go
+++ integration_test/integration_suite_test.go
@@ -53,13 +53,9 @@ var _ = AfterEach(func() {
 })
 
 var _ = SynchronizedBeforeSuite(func() []byte {
-	path := "/credhub-bin/credhub"
-	if _, err := os.Stat("/credhub-bin/credhub"); err != nil {
-		if os.IsNotExist(err) {
-			path, err = Build("code.cloudfoundry.org/credhub-cli")
-			Expect(err).NotTo(HaveOccurred())
-		}
-	}
+	path, err := Build("code.cloudfoundry.org/credhub-cli")
+	Expect(err).NotTo(HaveOccurred())
+
 	return []byte(path)
 }, func(data []byte) {
 	CommandPath = string(data)
