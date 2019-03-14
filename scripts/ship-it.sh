835221f604abd160c30e46a6d0ce44b24a1f7ee1
Mark DeLillo <mdelillo@pivotal.io>
2019-03-14 16:21:03 -0400

    Add ship-it script for consistency
    Signed-off-by: Tom Kennedy <tkennedy@pivotal.io>

---
+++ scripts/ship-it.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+function set_bash_error_handling() {
+    set -euo pipefail
+}
+
+function go_to_project_root_directory() {
+    local -r script_dir=$( dirname "${BASH_SOURCE[0]}")
+
+    cd "$script_dir/.."
+}
+
+function check_ssh_key() {
+    if ! ssh-add -l >/dev/null; then
+        echo "No SSH key loaded! Please run vkl."
+        exit 1
+    fi
+}
+
+function push_code() {
+    git push
+}
+
+function display_ascii_success_message() {
+    echo "Success!"
+}
+
+function main() {
+    set_bash_error_handling
+    go_to_project_root_directory
+    check_ssh_key
+
+    push_code
+    display_ascii_success_message
+}
+
+main
