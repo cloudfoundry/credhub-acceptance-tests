5b7867614ce50b9f0cab6f66a9fb4857b7b52f2e
Victoria Henry <vhenry@pivotal.io>
2019-06-18 16:20:19 -0400

    Test for `no-overwrite` and `converge` modes
    Signed-off-by: David Lohle <Proplex@users.noreply.github.com>

--- remote_backend/generate_test.go
+++ remote_backend/generate_test.go
@@ -14,88 +14,143 @@ import (
 
 var _ = Describe("Generate", func() {
 	Describe("generate", func() {
-		Describe("password type", func() {
-			It("generates password type", func() {
+		Context("when mode is set to overwrite", func() {
+			Context("password type", func() {
+				It("generates password type", func() {
+					name := "/some-password"
+
+					session := RunCommand("generate", "-t", "password", "-n", name, "-l", "10")
+					Expect(session).Should(Exit(0))
+
+					stdOut := string(session.Out.Contents())
+					Expect(stdOut).To(ContainSubstring(name))
+					Expect(stdOut).To(ContainSubstring("value: <redacted>"))
+
+					session = RunCommand("get", "-n", name, "-q")
+					stdOut = string(session.Out.Contents())
+					Expect(strings.TrimSpace(stdOut)).To(HaveLen(10))
+				})
+			})
+			Context("certificate type", func() {
+				It("generates certificate type", func() {
+					name := "/some-cert"
+
+					session := RunCommand("generate", "-t", "certificate", "-n", name, "-k", "4096", "-c", "test-cert", "--self-sign")
+					Expect(session).Should(Exit(0))
+
+					stdOut := string(session.Out.Contents())
+					Expect(stdOut).To(ContainSubstring(name))
+					Expect(stdOut).To(ContainSubstring("value: <redacted>"))
+
+					session = RunCommand("get", "-n", name)
+					stdOut = string(session.Out.Contents())
+					cert := CertFromPem(stdOut, false)
+					Expect(cert.Subject.CommonName).To(Equal("test-cert"))
+					Expect(cert.PublicKey.(*rsa.PublicKey).N.BitLen()).To(Equal(4096))
+				})
+			})
+			Context("rsa type", func() {
+				It("generates rsa type", func() {
+					name := "/some-rsa"
+
+					session := RunCommand("generate", "-t", "rsa", "-n", name, "-k", "4096")
+					Expect(session).Should(Exit(0))
+
+					stdOut := string(session.Out.Contents())
+					Expect(stdOut).To(ContainSubstring(name))
+					Expect(stdOut).To(ContainSubstring("value: <redacted>"))
+
+					session = RunCommand("get", "-n", name)
+					stdOut = string(session.Out.Contents())
+					Expect(stdOut).To(MatchRegexp(`private_key: |\s+-----BEGIN RSA PRIVATE KEY-----`))
+				})
+			})
+			Context("ssh type", func() {
+				It("generates ssh type", func() {
+					name := "/some-ssh"
+
+					session := RunCommand("generate", "-t", "ssh", "-n", name, "-k", "4096")
+					Expect(session).Should(Exit(0))
+
+					stdOut := string(session.Out.Contents())
+					Expect(stdOut).To(ContainSubstring(name))
+					Expect(stdOut).To(ContainSubstring("value: <redacted>"))
+
+					session = RunCommand("get", "-n", name)
+					stdOut = string(session.Out.Contents())
+					Expect(stdOut).To(MatchRegexp(`private_key: |\s+-----BEGIN SSH PRIVATE KEY-----`))
+				})
+			})
+			Context("user type", func() {
+				It("generates user type", func() {
+					name := "/some-user"
+
+					session := RunCommand("generate", "-t", "user", "-n", name, "-l", "47", "-U")
+					Expect(session).Should(Exit(0))
+
+					stdOut := string(session.Out.Contents())
+					Expect(stdOut).To(ContainSubstring(name))
+					Expect(stdOut).To(ContainSubstring("value: <redacted>"))
+
+					session = RunCommand("get", "-n", name, "-k", "password", "-q")
+					stdOut = string(session.Out.Contents())
+					Expect(strings.TrimSpace(stdOut)).To(HaveLen(47))
+					Expect(strings.TrimSpace(stdOut)).To(MatchRegexp(`^[a-z0-9_\-]+$`))
+
+				})
+			})
+		})
+		Context("when mode is set to no overwrite", func() {
+			It("does not regenerate credential", func() {
 				name := "/some-password"
 
 				session := RunCommand("generate", "-t", "password", "-n", name, "-l", "10")
 				Expect(session).Should(Exit(0))
 
-				stdOut := string(session.Out.Contents())
-				Expect(stdOut).To(ContainSubstring(name))
-				Expect(stdOut).To(ContainSubstring("value: <redacted>"))
-
 				session = RunCommand("get", "-n", name, "-q")
-				stdOut = string(session.Out.Contents())
-				Expect(strings.TrimSpace(stdOut)).To(HaveLen(10))
+				oldStdOut := strings.TrimSpace(string(session.Out.Contents()))
+
+
+				generationParameters := `{"name": "/some-password", "type": "password", "mode": "no-overwrite"}`
+				session = RunCommand("curl", "-p", "api/v1/data", "-X", "POST", "-d", generationParameters)
+				Expect(session).Should(Exit(0))
+				Expect(string(session.Out.Contents())).ToNot(ContainSubstring(oldStdOut))
 			})
 		})
-		Describe("certificate type", func() {
-			It("generates certificate type", func() {
-				name := "/some-cert"
 
-				session := RunCommand("generate", "-t", "certificate", "-n", name, "-k", "4096", "-c", "test-cert", "--self-sign")
-				Expect(session).Should(Exit(0))
+		Context("when mode is set to converge", func() {
+			Context("and generation parameters are the same", func() {
+				It("does not regenerate credential", func() {
+					name := "/some-password"
 
-				stdOut := string(session.Out.Contents())
-				Expect(stdOut).To(ContainSubstring(name))
-				Expect(stdOut).To(ContainSubstring("value: <redacted>"))
+					session := RunCommand("generate", "-t", "password", "-n", name, "-l", "10")
+					Expect(session).Should(Exit(0))
 
-				session = RunCommand("get", "-n", name)
-				stdOut = string(session.Out.Contents())
-				cert := CertFromPem(stdOut, false)
-				Expect(cert.Subject.CommonName).To(Equal("test-cert"))
-				Expect(cert.PublicKey.(*rsa.PublicKey).N.BitLen()).To(Equal(4096))
+					session = RunCommand("get", "-n", name, "-q")
+					oldStdOut := strings.TrimSpace(string(session.Out.Contents()))
+
+					generationParameters := `{"name": "/some-password", "type": "password", "mode": "converge", "parameters":{"length":10}}`
+					session = RunCommand("curl", "-p", "api/v1/data", "-X", "POST", "-d", generationParameters)
+					Expect(session).Should(Exit(0))
+					Expect(string(session.Out.Contents())).To(ContainSubstring(oldStdOut))
+				})
 			})
-		})
-		Describe("rsa type", func() {
-			It("generates rsa type", func() {
-				name := "/some-rsa"
+			Context("and generation parameters are not the same", func() {
+				It("regenerates the credential", func() {
+					name := "/some-password"
 
-				session := RunCommand("generate", "-t", "rsa", "-n", name, "-k", "4096")
-				Expect(session).Should(Exit(0))
+					session := RunCommand("generate", "-t", "password", "-n", name, "-l", "10")
+					Expect(session).Should(Exit(0))
 
-				stdOut := string(session.Out.Contents())
-				Expect(stdOut).To(ContainSubstring(name))
-				Expect(stdOut).To(ContainSubstring("value: <redacted>"))
+					session = RunCommand("get", "-n", name, "-q")
+					oldStdOut := strings.TrimSpace(string(session.Out.Contents()))
 
-				session = RunCommand("get", "-n", name)
-				stdOut = string(session.Out.Contents())
-				Expect(stdOut).To(MatchRegexp(`private_key: |\s+-----BEGIN RSA PRIVATE KEY-----`))
-			})
-		})
-		Describe("ssh type", func() {
-			It("generates ssh type", func() {
-				name := "/some-ssh"
 
-				session := RunCommand("generate", "-t", "ssh", "-n", name, "-k", "4096")
-				Expect(session).Should(Exit(0))
-
-				stdOut := string(session.Out.Contents())
-				Expect(stdOut).To(ContainSubstring(name))
-				Expect(stdOut).To(ContainSubstring("value: <redacted>"))
-
-				session = RunCommand("get", "-n", name)
-				stdOut = string(session.Out.Contents())
-				Expect(stdOut).To(MatchRegexp(`private_key: |\s+-----BEGIN SSH PRIVATE KEY-----`))
-			})
-		})
-		Describe("user type", func() {
-			It("generates user type", func() {
-				name := "/some-user"
-
-				session := RunCommand("generate", "-t", "user", "-n", name, "-l", "47", "-U")
-				Expect(session).Should(Exit(0))
-
-				stdOut := string(session.Out.Contents())
-				Expect(stdOut).To(ContainSubstring(name))
-				Expect(stdOut).To(ContainSubstring("value: <redacted>"))
-
-				session = RunCommand("get", "-n", name, "-k", "password", "-q")
-				stdOut = string(session.Out.Contents())
-				Expect(strings.TrimSpace(stdOut)).To(HaveLen(47))
-				Expect(strings.TrimSpace(stdOut)).To(MatchRegexp(`^[a-z0-9_\-]+$`))
-
+					generationParameters := `{"name": "/some-password", "type": "password", "mode": "converge"}`
+					session = RunCommand("curl", "-p", "api/v1/data", "-X", "POST", "-d", generationParameters)
+					Expect(session).Should(Exit(0))
+					Expect(string(session.Out.Contents())).ToNot(ContainSubstring(oldStdOut))
+				})
 			})
 		})
 	})
