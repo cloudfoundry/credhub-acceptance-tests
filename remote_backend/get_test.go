3d0a4d163292fd24f997bab26de7c654bd67fb3a
Elenore Bastian <ebastian@pivotal.io>
2019-07-23 15:08:41 -0400

    Add tests for getting multiple versions from a remote backend
    [#167111096]
    
    Signed-off-by: Marty Spiewak <mspiewak@pivotal.io>

--- remote_backend/get_test.go
+++ remote_backend/get_test.go
@@ -1,6 +1,7 @@
 package remote_backend_test
 
 import (
+	"fmt"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	. "github.com/onsi/gomega/gexec"
@@ -17,6 +18,34 @@ var _ = Describe("Get", func() {
 			stdOut := string(session.Out.Contents())
 			Expect(stdOut).To(ContainSubstring("my-value"))
 			Expect(stdOut).To(ContainSubstring("test-value"))
+		})
+		Context("getting all versions", func() {
+			It("gets all versions of the credential", func() {
+				credentialName := GenerateUniqueCredentialName()
+				session := RunCommand("set", "-n", credentialName, "-t", "password", "--password", "first-password")
+				Eventually(session).Should(Exit(0))
+				session = RunCommand("set", "-n", credentialName, "-t", "password", "--password", "second-password")
+				Eventually(session).Should(Exit(0))
+
+				session = RunCommand("curl", "-p", fmt.Sprintf("/api/v1/data?name=%s", credentialName))
+				stdOut := string(session.Out.Contents())
+				Expect(stdOut).To(ContainSubstring(`"value": "second-password"`))
+				Expect(stdOut).To(ContainSubstring(`"value": "first-password"`))
+			})
+		})
+		Context("getting multiple versions", func() {
+			It("gets the desired number of versions of the credential", func() {
+				credentialName := GenerateUniqueCredentialName()
+				session := RunCommand("set", "-n", credentialName, "-t", "password", "--password", "first-password")
+				Eventually(session).Should(Exit(0))
+				session = RunCommand("set", "-n", credentialName, "-t", "password", "--password", "second-password")
+				Eventually(session).Should(Exit(0))
+
+				session = RunCommand("curl", "-p", fmt.Sprintf("/api/v1/data?name=%s&versions=1", credentialName))
+				stdOut := string(session.Out.Contents())
+				Expect(stdOut).To(ContainSubstring(`"value": "second-password"`))
+				Expect(stdOut).NotTo(ContainSubstring(`"value": "first-password"`))
+			})
 		})
 	})
 	Describe("get by id", func() {
