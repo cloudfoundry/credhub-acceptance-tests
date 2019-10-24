7194dec40c9f77eb481219a661700e009a110d90
Marty Spiewak <mspiewak@pivotal.io>
2019-10-24 13:50:09 -0400

    Update bbr_integration_test to test all credential types
    [#169240261]

--- bbr_integration_test/bbr_integration_test.go
+++ bbr_integration_test/bbr_integration_test.go
@@ -3,13 +3,14 @@ package bbr_integration
 import (
 	"fmt"
 
+	"io/ioutil"
+	"os"
+
 	"github.com/cloudfoundry-incubator/credhub-acceptance-tests/test_helpers"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/gbytes"
 	. "github.com/onsi/gomega/gexec"
-	"io/ioutil"
-	"os"
 )
 
 var _ = Describe("Backup and Restore", func() {
@@ -37,8 +38,11 @@ var _ = Describe("Backup and Restore", func() {
 		os.RemoveAll(bbrDirectory)
 	})
 
-	It("Successfully backs up and restores a Credhub release", func() {
-		By("adding a test credential")
+	It("restores credentials from a backup", func() {
+		By("creating test credentials")
+		credhubCredentialNames := createCredhubCredentials(bbrTestPath)
+
+		By("adding a test password that will be edited after backup")
 		session := RunCommand("credhub", "set", "--name", credentialName, "--type", "password", "-w", "originalsecret")
 		Eventually(session).Should(Exit(0))
 
@@ -50,13 +54,19 @@ var _ = Describe("Backup and Restore", func() {
 		session = RunCommand("sh", "-c", fmt.Sprintf("tar tf %s/%s*/*credhubdb.tar ./credhubdb_dump", bbrDirectory, config.DeploymentName))
 		Eventually(session).Should(Exit(0))
 
-		By("editing the test credential")
+		By("editing the test password")
 		session = RunCommand("credhub", "set", "--name", credentialName, "--type", "password", "-w", "updatedsecret")
 		Eventually(session).Should(Exit(0))
 
 		editSession := RunCommand("credhub", "get", "--name", credentialName)
 		Eventually(editSession).Should(Exit(0))
 		Eventually(editSession.Out).Should(gbytes.Say("value: updatedsecret"))
+
+		By("deleting all of the other test credentials")
+		for _, credentialName := range credhubCredentialNames {
+			session = RunCommand("credhub", "delete", "-n", credentialName)
+			Eventually(session).Should(Exit(0))
+		}
 
 		By("running bbr restore")
 		session = RunCommand("sh", "-c",
@@ -65,12 +75,54 @@ var _ = Describe("Backup and Restore", func() {
 		)
 		Eventually(session).Should(Exit(0))
 
-		By("checking if the test credentials was restored")
+		By("checking if the test password was restored")
 		getSession := RunCommand("credhub", "get", "--name", credentialName)
 		Eventually(getSession).Should(Exit(0))
 		Eventually(getSession.Out).Should(gbytes.Say("value: originalsecret"))
+
+		By("checking if the other test credentials were restored")
+		findSession := RunCommand("credhub", "find")
+		Eventually(findSession).Should(Exit(0))
+		for _, credentialName := range credhubCredentialNames {
+			Expect(findSession.Out.Contents()).To(ContainSubstring(credentialName))
+		}
 	})
 })
+
+func createCredhubCredentials(credentialPrefix string) []string {
+	passwordName := fmt.Sprintf("%s/%s", credentialPrefix, test_helpers.GenerateUniqueCredentialName())
+	certificateName := fmt.Sprintf("%s/%s", credentialPrefix, test_helpers.GenerateUniqueCredentialName())
+	sshName := fmt.Sprintf("%s/%s", credentialPrefix, test_helpers.GenerateUniqueCredentialName())
+	rsaName := fmt.Sprintf("%s/%s", credentialPrefix, test_helpers.GenerateUniqueCredentialName())
+	jsonName := fmt.Sprintf("%s/%s", credentialPrefix, test_helpers.GenerateUniqueCredentialName())
+	valueName := fmt.Sprintf("%s/%s", credentialPrefix, test_helpers.GenerateUniqueCredentialName())
+	userName := fmt.Sprintf("%s/%s", credentialPrefix, test_helpers.GenerateUniqueCredentialName())
+
+	session := RunCommand("credhub", "generate", "--name", passwordName, "--type", "password")
+	Eventually(session).Should(Exit(0))
+	session = RunCommand("credhub", "generate", "--name", certificateName, "--type", "certificate", "-c", "cn", "--is-ca")
+	Eventually(session).Should(Exit(0))
+	session = RunCommand("credhub", "generate", "--name", sshName, "--type", "ssh")
+	Eventually(session).Should(Exit(0))
+	session = RunCommand("credhub", "generate", "--name", rsaName, "--type", "rsa")
+	Eventually(session).Should(Exit(0))
+	session = RunCommand("credhub", "generate", "--name", userName, "--type", "user")
+	Eventually(session).Should(Exit(0))
+	session = RunCommand("credhub", "set", "--name", jsonName, "--type", "json", "-v", `{"test": "secret"}`)
+	Eventually(session).Should(Exit(0))
+	session = RunCommand("credhub", "set", "--name", valueName, "--type", "value", "-v", "some-value")
+	Eventually(session).Should(Exit(0))
+
+	return []string{
+		passwordName,
+		certificateName,
+		sshName,
+		valueName,
+		jsonName,
+		rsaName,
+		userName,
+	}
+}
 
 func CleanupCredhub(path string) {
 	By("Cleaning up credhub bbr test passwords")
