cea467e209a112b18726ef4a45d8877941a5b66a
Mia Fryling <mfryling@pivotal.io>
2020-05-29 09:30:03 -0700

    Add tests for delete creds by name & path [#171446867]
    Co-authored-by: Peter Chen <pchen@pivotal.io>

---
+++ integration_test/delete_test.go
@@ -0,0 +1,41 @@
+package integration_test
+
+import (
+	. "github.com/cloudfoundry-incubator/credhub-acceptance-tests/test_helpers"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/gexec"
+)
+
+var _ = Describe("Delete Test", func() {
+	It("should delete credentials by name", func() {
+		RunCommand("generate", "-n", "/a/cred1", "-t", "password")
+
+		session := RunCommand("delete", "-n", "/a/cred1")
+		Eventually(session).Should(Exit(0))
+
+		session = RunCommand("get", "-n", "/a/cred1")
+		Eventually(session).Should(Exit(1))
+		Expect(string(session.Err.Contents())).To(ContainSubstring("The request could not be completed because the credential does not exist or you do not have sufficient authorization."))
+	})
+
+	It("should delete credentials by path", func() {
+		RunCommand("generate", "-n", "/a", "-t", "password")
+		RunCommand("generate", "-n", "/a/cred1", "-t", "password")
+		RunCommand("generate", "-n", "/a/b/cred2", "-t", "password")
+		RunCommand("generate", "-n", "/a/b/cred3", "-t", "password")
+
+		session := RunCommand("delete", "-p", "/a")
+		Eventually(session).Should(Exit(0))
+
+		session = RunCommand("find", "-p", "/a")
+		Eventually(session).Should(Exit(0))
+		stdOut := string(session.Out.Contents())
+		Expect(stdOut).To(ContainSubstring("credentials: []"))
+
+		session = RunCommand("get", "-n", "/a")
+		Eventually(session).Should(Exit(0))
+		stdOut = string(session.Out.Contents())
+		Expect(stdOut).To(ContainSubstring("/a"))
+	})
+})
