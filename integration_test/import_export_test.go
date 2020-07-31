03f2e92b6e1d78260eba50c8a22c6e716abc1c90
Bruce Ricard <bricard@vmware.com>
2020-07-31 16:46:21 -0700

    import export of certificate credentials
    When importing an export file
    It should restore the exported credentials
    
    * Add test coverage for work in credhub-cli:
    https://github.com/cloudfoundry-incubator/credhub-cli/commit/fb2d6a0e950926faef2efda88c28a3a07d79622c
    
    [#174125828](https://www.pivotaltracker.com/story/show/174125828)
    
    Co-authored-by: Aidan Obley <aobley@vmware.com>

---
+++ integration_test/import_export_test.go
@@ -0,0 +1,117 @@
+package integration_test
+
+import (
+	. "github.com/cloudfoundry-incubator/credhub-acceptance-tests/test_helpers"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/gexec"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"os"
+)
+
+var _ = Describe("Import/Export test", func() {
+
+	When("Importing an export file", func() {
+		const (
+			credentialRootPath               = "/bruce"
+			selfSignedRootCACredPath         = credentialRootPath + "/ca"
+			rootSignedLeafCredPath           = credentialRootPath + "/ca-leaf"
+			rootSignedIntermediateCACredPath = credentialRootPath + "/int"
+			intermediateSignedLeafCredPath   = credentialRootPath + "/int-leaf"
+		)
+
+		var (
+			session    *Session
+			exportFile *os.File
+		)
+
+		BeforeEach(func() {
+			var err error
+			exportFile, err = ioutil.TempFile("", "export-data")
+			Expect(err).NotTo(HaveOccurred())
+
+			session = RunCommand("generate",
+				"--name", selfSignedRootCACredPath,
+				"--type", "certificate",
+				"--self-sign",
+				"--is-ca",
+				"--common-name", "bruce-ca",
+			)
+			Expect(session).To(Exit(0))
+
+			session = RunCommand("generate",
+				"--name", rootSignedIntermediateCACredPath,
+				"--type", "certificate",
+				"--ca", selfSignedRootCACredPath,
+				"--is-ca",
+				"--common-name", "bruce-int",
+			)
+			Expect(session).To(Exit(0))
+
+			session = RunCommand("generate",
+				"--name", rootSignedLeafCredPath,
+				"--type", "certificate",
+				"--ca", selfSignedRootCACredPath,
+				"--common-name", "bruce-leaf1",
+			)
+			Expect(session).To(Exit(0))
+
+			session = RunCommand("generate",
+				"--name", intermediateSignedLeafCredPath,
+				"--type", "certificate",
+				"--ca", rootSignedIntermediateCACredPath,
+				"--common-name", "bruce-leaf2",
+			)
+			Expect(session).To(Exit(0))
+		})
+
+		AfterEach(func() {
+			session = RunCommand("delete",
+				"--path", credentialRootPath,
+			)
+			Expect(session).To(Exit(0))
+		})
+
+		It("should restore the exported credentials", func() {
+			selfSignedRootCA := getTrimmedCertificateForComparison(selfSignedRootCACredPath)
+			rootSignedIntermediateCA := getTrimmedCertificateForComparison(rootSignedIntermediateCACredPath)
+			rootSignedLeaf := getTrimmedCertificateForComparison(rootSignedLeafCredPath)
+			intermediateSignedLeaf := getTrimmedCertificateForComparison(intermediateSignedLeafCredPath)
+
+			session = RunCommand("export",
+				"--path", credentialRootPath,
+				"--file", exportFile.Name(),
+			)
+			Expect(session).To(Exit(0))
+
+			session = RunCommand("delete",
+				"--path", credentialRootPath,
+			)
+			Expect(session).To(Exit(0))
+
+			session = RunCommand("import",
+				"-f", exportFile.Name(),
+			)
+			Expect(session).To(Exit(0))
+
+			Expect(getTrimmedCertificateForComparison(selfSignedRootCACredPath)).To(Equal(selfSignedRootCA))
+			Expect(getTrimmedCertificateForComparison(rootSignedIntermediateCACredPath)).To(Equal(rootSignedIntermediateCA))
+			Expect(getTrimmedCertificateForComparison(rootSignedLeafCredPath)).To(Equal(rootSignedLeaf))
+			Expect(getTrimmedCertificateForComparison(intermediateSignedLeafCredPath)).To(Equal(intermediateSignedLeaf))
+		})
+	})
+})
+
+	func getTrimmedCertificateForComparison(name string) map[string]interface{}{
+		session = RunCommand("get", "--name", name)
+		Expect(session).To(Exit(0))
+
+		certificate := make(map[string]interface{})
+		err := yaml.Unmarshal(session.Out.Contents(), certificate)
+		Expect(err).NotTo(HaveOccurred())
+		delete(certificate, "version_created_at")
+		delete(certificate, "id")
+
+		return certificate
+	}
