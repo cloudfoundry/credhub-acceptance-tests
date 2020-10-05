29f713a01f40a491bba99e0a28e17c20eb4209bc
Aidan Obley <aobley@vmware.com>
2020-10-05 14:59:42 -0700

    Fix test by substring match
    - cli now outputs stderr in color, which puts color chars
    around error message; hence the test should perform a
    substring match to ignore the color chars
    
    Co-authored-by: Peter Chen <peterch@vmware.com>

--- integration_test/certificates_test.go
+++ integration_test/certificates_test.go
@@ -380,7 +380,7 @@ var _ = Describe("Certificates Test", func() {
 					stdErr := strings.TrimSpace(string(session.Err.Contents()))
 
 					Eventually(session).Should(Exit(1))
-					Expect(stdErr).To(Equal("Private key is malformed. Key file does not contain an RSA private key"))
+					Expect(stdErr).To(ContainSubstring("Private key is malformed. Key file does not contain an RSA private key"))
 				})
 			})
 			Context("and is encrypted", func() {
