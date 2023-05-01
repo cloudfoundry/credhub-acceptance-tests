55527e22c6d44d70459ede8296930fad67a6984e
Peter Chen <peterch@vmware.com>
2023-05-01 13:36:39 -0700

    fix CI error: "no such file or directory" when loading test config
    - error:
    ```
    • Failure in Spec Setup (BeforeEach) [0.001 seconds]
    [BeforeEach] Certificates Test setting a certificate when private key format is PKCS1 should require a certificate type
    /tmp/build/6a8cf187/credhub-acceptance-tests/integration_test/certificates_test.go:320
    
      Unexpected error:
          <*fs.PathError | 0xc0004c0ed0>:
          open /tmp/build/6a8cf187/credhub-acceptance-tests/integration_test/test_config.json: no such file or directory
          {
              Op: "open",
              Path: "/tmp/build/6a8cf187/credhub-acceptance-tests/integration_test/test_config.json",
              Err: <syscall.Errno>0x2,
          }
      occurred
    ```
    - The test config loading code is now looking for the test config json
    under `/credhub-acceptance-tests/TEST-DIR/test_config.json` when the
    json file is created at the root dir
    (`/credhub-acceptance-tests/test_config.json`). This is likely because
    of our golang bump to 1.19 in CI; golang 1.19 likely has a change that
    affects the evaluation of `PWD` (see: https://tip.golang.org/doc/go1.19)
    that causes this failure. `PWD` now seems to return a more specific
    value.
    - This commit attempts to fix the issue by adjusting the config file
      loading path.

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -4,27 +4,27 @@ set -eu
 
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. && pwd )"
 
-API_URL=${API_URL:-https://localhost:9000}
-USERNAME=${USERNAME:-credhub}
-PASSWORD=${PASSWORD:-password}
-CREDENTIAL_ROOT=${CREDENTIAL_ROOT:-~/workspace/credhub-release/src/credhub/applications/credhub-api/src/test/resources}
-UAA_CA=${UAA_CA:-~/workspace/credhub-deployments/ca/uaa_ca.pem}
-CLIENT_NAME=${CLIENT_NAME:-credhub_client}
-CLIENT_SECRET=${CLIENT_SECRET:-secret}
-CONCATENATE_CAS=${CONCATENATE_CAS:-true}
-
-cat <<EOF > test_config.json
-{
-  "api_url": "${API_URL}",
-  "api_username":"${USERNAME}",
-  "api_password":"${PASSWORD}",
-  "credential_root":"${CREDENTIAL_ROOT}",
-  "uaa_ca":"${UAA_CA}",
-  "client_name":"${CLIENT_NAME}",
-  "client_secret":"${CLIENT_SECRET}",
-  "concatenate_cas":${CONCATENATE_CAS}
-}
-EOF
+#API_URL=${API_URL:-https://localhost:9000}
+#USERNAME=${USERNAME:-credhub}
+#PASSWORD=${PASSWORD:-password}
+#CREDENTIAL_ROOT=${CREDENTIAL_ROOT:-~/workspace/credhub-release/src/credhub/applications/credhub-api/src/test/resources}
+#UAA_CA=${UAA_CA:-~/workspace/credhub-deployments/ca/uaa_ca.pem}
+#CLIENT_NAME=${CLIENT_NAME:-credhub_client}
+#CLIENT_SECRET=${CLIENT_SECRET:-secret}
+#CONCATENATE_CAS=${CONCATENATE_CAS:-true}
+#
+#cat <<EOF > test_config.json
+#{
+#  "api_url": "${API_URL}",
+#  "api_username":"${USERNAME}",
+#  "api_password":"${PASSWORD}",
+#  "credential_root":"${CREDENTIAL_ROOT}",
+#  "uaa_ca":"${UAA_CA}",
+#  "client_name":"${CLIENT_NAME}",
+#  "client_secret":"${CLIENT_SECRET}",
+#  "concatenate_cas":${CONCATENATE_CAS}
+#}
+#EOF
 
 pushd "$BASEDIR" >/dev/null
   ginkgo -r -p -skipPackage bbr_integration_test,remote_backend -randomizeAllSpecs -randomizeSuites "$@"
